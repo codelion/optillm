0c82a6f47b260c71f7f2ccb5ba9f03ed24a50304
Attila Vass <vanetreg@gmail.com>
2024-10-08 09:22:48 +0200

    v3

--- va_app.py
+++ va_app.py
@@ -22,24 +22,10 @@ OLLAMA_BASE_URL = os.environ.get("OLLAMA_BASE_URL")
 # )
 
 # calling Openrouter's API
-client = OpenAI(api_key=OPENROUTER_API_KEY, base_url=OPENROUTER_BASE_URL)
-
-response = client.chat.completions.create(
-  model="moa-nousresearch/hermes-3-llama-3.1-405b:free",
-  messages=[
-    {
-      "role": "user",
-      "content": "How many Rs are there in the word strawberry?"
-    }
-  ],
-  temperature=0.2
-)
-
-# calling Ollama API on Lightning
-# client = OpenAI(api_key="any", base_url=OLLAMA_BASE_URL)
+# client = OpenAI(api_key=OPENROUTER_API_KEY, base_url=OPENROUTER_BASE_URL)
 
 # response = client.chat.completions.create(
-#   model="moa-ollama/llama3.2",
+#   model="moa-nousresearch/hermes-3-llama-3.1-405b:free",
 #   messages=[
 #     {
 #       "role": "user",
@@ -48,6 +34,20 @@ response = client.chat.completions.create(
 #   ],
 #   temperature=0.2
 # )
+
+# calling Ollama API on Lightning
+client = OpenAI(api_key="any", base_url=OLLAMA_BASE_URL)
+
+response = client.chat.completions.create(
+  model="moa-ollama/llama3.2",
+  messages=[
+    {
+      "role": "user",
+      "content": "How many Rs are there in the word strawberry?"
+    }
+  ],
+  temperature=0.2
+)
 
 print(response.choices[0].message.content)
 # print(response)
