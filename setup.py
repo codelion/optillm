a29749468aa299975a42ec1661c87d62bfaef936
Asankhaya Sharma <codelion@users.noreply.github.com>
2024-10-24 14:53:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="optillm",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[
         "numpy",
