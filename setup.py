52d67d5be7a7e8a8ce648686d6f2ac62fc92541e
Asankhaya Sharma <codelion@users.noreply.github.com>
2024-10-24 15:33:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="optillm",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(),
     py_modules=['optillm'],
     install_requires=[
