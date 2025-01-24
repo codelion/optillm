63f6faffa92279339271f464db7e1956849c6687
Asankhaya Sharma <codelion@users.noreply.github.com>
2025-01-24 17:14:31 +0800

    Update setup.py
    bump version for new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="optillm",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     py_modules=['optillm'],
     package_data={
